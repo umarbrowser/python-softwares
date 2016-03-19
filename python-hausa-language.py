377c1cebb6263dbbb005a99d21025cc356f5d21d
pycoder_boy <umarbrowser20@gmail.com>
2016-03-19 10:37:42 +0100

    Create python-hausa-language.py

---
+++ python-hausa-language.py
@@ -0,0 +1,6 @@
+suna = raw_input("meye sunanka")
+quest = raw_input("meye burinka")
+color = raw_input("wani color kafi so")
+
+print"Yawwa, kace sunanka %s, burinka %s," \
+"kuma color dakafi so %s." % (suna, quest, color)
